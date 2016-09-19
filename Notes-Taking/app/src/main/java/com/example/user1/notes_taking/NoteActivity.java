ca7dd7f24098407189cc06a7d6bef4fdcf1251dc
Tomer Mardan <USER1@DOMAIN.local>
2016-09-19 15:17:45 +0300

    more production

--- Notes-Taking/app/src/main/java/com/example/user1/notes_taking/NoteActivity.java
+++ Notes-Taking/app/src/main/java/com/example/user1/notes_taking/NoteActivity.java
@@ -1,17 +1,51 @@
 package com.example.user1.notes_taking;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NoteActivity extends AppCompatActivity {
     Intent intent;
     boolean newNote;
+    EditText etTitle, etText;
+    LocalNoteService lns = new LocalNoteService(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
         intent = getIntent();
+        etTitle = (EditText) findViewById(R.id.etTitle);
+        etText = (EditText) findViewById(R.id.etText);
+
 
     }
+    class CreateNewNote extends AsyncTask<String,Object,Object>
+    {
+
+        @Override
+        protected Integer doInBackground(String... params) {
+
+            String title =params[0];
+            String text =params[1];
+            lns.createNewNote(title,text);
+            return null;
+        }
+    }
+    class EditNote extends AsyncTask<String,Object,Object>
+    {
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            String id =params[0];
+            String title = (String)params[1];
+            String text = (String)params[2];
+            lns.editNote(id,title,text);
+            return null;
+        }
+    }
+
+    
+
 }
