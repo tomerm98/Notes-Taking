016e3b538ec386617f553755480fb32887517249
Tomer Mardan <tomer@mardan.co.il>
2016-10-13 06:23:33 +0300

    ...

--- Notes-Taking/app/src/main/java/com/example/user1/notes_taking/CloudNoteService.java
+++ Notes-Taking/app/src/main/java/com/example/user1/notes_taking/CloudNoteService.java
@@ -1,7 +1,11 @@
 package com.example.user1.notes_taking;
 
 import android.content.Context;
+import android.support.design.widget.TabLayout;
 
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
@@ -14,6 +18,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -22,41 +27,53 @@ import java.util.Random;
 public class CloudNoteService implements NoteServiceInterface{
     private final String ID_FILE_NAME = "id.txt";
     private final String TABLE_ID = "Notes";
-    private final int idLength = 50;
+    private final int ID_LENGTH = 50;
     private ParseObject po;
     private String userId;
     private Context context;
+
 
     public CloudNoteService(Context context) throws IOException {
         this.context = context;
 
         File[] files = context.getFilesDir().listFiles();
         userId = "";
-        for (File f : files) {
-            if (f.getName().equals(ID_FILE_NAME)) ;
-            {
-                userId = "";
-                FileInputStream in = context.openFileInput(ID_FILE_NAME);
-                InputStreamReader inputStreamReader = new InputStreamReader(in);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    userId += line;
-                }
-
+        for (File f : files)
+            if (f.getName().equals(ID_FILE_NAME)) {
+                userId = readAllTextFromFile(ID_FILE_NAME, this.context);
+                break;
             }
-        }
-        if (userId.equals(""))
-        {
-            userId = generateRandomId(idLength);
-            FileOutputStream fos = context.openFileOutput(ID_FILE_NAME, Context.MODE_PRIVATE);
-            PrintWriter pw = new PrintWriter(fos);
-            pw.print(userId);
-            pw.flush();
-            pw.close();
+
+        if (userId.equals("")) {
+            userId = generateRandomId(ID_LENGTH);
+            writeToFile(userId,ID_FILE_NAME,this.context);
         }
 
+        Parse.initialize(new Parse.Configuration.Builder(this.context)
+                .applicationId("hOWu3YDCSWNu8TIKGfAX3ApSuI1i05Sy7IP7KPnY")
+                .clientKey("BVgfoPAgU7wMU2vPqmv0p8ToRsesxuGqhdB0OvRe")
+                .server("https://parseapi.back4app.com/").build()
+        );
 
+
+    }
+    private void writeToFile(String text, String fileName, Context context) throws FileNotFoundException {
+        FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+        PrintWriter pw = new PrintWriter(fos);
+        pw.print(text);
+        pw.close();
+    }
+
+    private String readAllTextFromFile(String fileName, Context context) throws IOException {
+        String text = "";
+        FileInputStream in = context.openFileInput(fileName);
+        InputStreamReader inputStreamReader = new InputStreamReader(in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            text += line;
+        }
+        return text;
     }
 
     private String generateRandomId(int length) {
@@ -74,7 +91,16 @@ public class CloudNoteService implements NoteServiceInterface{
     @Override
     public void saveNote(Note n) {
         ParseQuery<ParseObject> query = ParseQuery.getQuery(TABLE_ID);
-
+        query.whereEqualTo("", "Dan Stemkoski");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> scoreList, ParseException e) {
+                if (e == null) {
+                   //Log.d("score", "Retrieved " + scoreList.size() + " scores");
+                } else {
+                //    Log.d("score", "Error: " + e.getMessage());
+                }
+            }
+        });
 
 
     }
